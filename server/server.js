c7d4f87e8bed73427e7207000147724452883a75
TechSmartSatyam <satyamsmartboy143@gamil.com>
2026-09-05 00:25:02 -0700

    Fix Request intity Too large

--- server/server.js
+++ server/server.js
@@ -40,12 +40,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(
   express.json({
+    limit: '10mb',
     verify: (req, res, buf) => {
       req.rawBody = buf.toString();
     },
   })
 );
-app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10mb',
+  })
+);
+
 app.use(cookieParser());
 
 // Initialize Socket.IO
