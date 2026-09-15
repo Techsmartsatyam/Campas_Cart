3aeb8cf97ea5524aed1529dc52cb711457ce556f
TechSmartSatyam <satyamsmartboy143@gamil.com>
2026-09-15 02:34:39 -0700

    Revert "Two shop"
    This reverts commit 4f81da07c77737c4d94a0fff0a7eac65ba8320d5.

--- client/src/pages/Student.jsx
+++ client/src/pages/Student.jsx
@@ -156,7 +156,7 @@ export default function Student() {
             No active shops available right now.
           </div>
         ) : (
-          <div className="shop-grid-responsive">
+          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(260px, 1fr))', gap: '1.25rem' }}>
             {shops.map((shopItem) => (
               <ShopCard
                 key={shopItem._id}
